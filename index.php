87c812b0d6060d7eff67a68f88629998fb54baf3
damiankoper <kopernickk@gmail.com>
2017-05-24 22:12:56 +0200

    extends & include added

--- index.php
+++ index.php
@@ -7,10 +7,15 @@ use \Tegs\core\Tegs_core;
 use \Tegs\loader\Tegs_loader;
 
 try {
+
     $loader = new Tegs_loader("tegs_templates/");
     $Tegs = new Tegs_core($loader);
-    $template = $Tegs->load("template.html.tegs");
-    $template->display(array("test"=>"damcio", "tablica"=>array("heh", "hih", "huh")));
+    $template = $Tegs->load("base.html.tegs");
+    $template->display(array("test"=>"damcio", "tablica"=>array("heh<p></p>", "hih", "huh")));
+    //$template->display_block("damian",array("test"=>"damcio", "tablica"=>array("heh<b>hehehe</b>", "hih", "huh")));
+
 } catch (Exception $e) {
-    echo "<b>Error ".$e->getFile()."(".$e->getLine()."): </b>". $e->getMessage();
+    echo "<b>Error ".end($e->getTrace())["file"]."(".end($e->getTrace())["line"]."): </b>". $e->getMessage();
 }
+?>
+<html
